491511626e21ba8cd1100cab81fcb7f59fbe9a6e
Anastasia <merlmorison@gmail.com>
2024-11-09 20:21:59 +0200

    В ПИЗДУ

---
+++ Assets/buttons/ColliderToggleManager.cs
@@ -0,0 +1,49 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ColliderToggleManager : UdonSharpBehaviour
+{
+    // Массив для хранения корневых объектов с коллайдерами
+    public GameObject[] targetObjects;
+
+    // Флаг для отслеживания состояния (включены или выключены коллайдеры)
+    [UdonSynced] // Синхронизация состояния между игроками
+    private bool isColliderEnabled = true;
+
+    // Метод для переключения состояния коллайдеров
+    public void ToggleColliders()
+    {
+        isColliderEnabled = !isColliderEnabled; // Инвертируем текущее состояние
+        UpdateCollidersState(); // Обновляем состояние коллайдеров
+    }
+
+    // Метод для обновления состояния коллайдеров на всех объектах
+    private void UpdateCollidersState()
+    {
+        foreach (GameObject obj in targetObjects)
+        {
+            if (obj != null)
+            {
+                // Включаем/выключаем коллайдеры у объекта и всех его дочерних объектов
+                ToggleCollidersInChildren(obj);
+            }
+        }
+    }
+
+    // Рекурсивный метод для включения/выключения коллайдеров у объекта и его дочерних объектов
+    private void ToggleCollidersInChildren(GameObject obj)
+    {
+        Collider[] colliders = obj.GetComponents<Collider>();
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = isColliderEnabled; // Включаем/выключаем каждый коллайдер
+        }
+
+        foreach (Transform child in obj.transform)
+        {
+            ToggleCollidersInChildren(child.gameObject);
+        }
+    }
+}
---
+++ Assets/buttons/ToggleColliderButton.cs
@@ -0,0 +1,19 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ToggleColliderButton : UdonSharpBehaviour
+{
+    // Ссылка на управляющий скрипт
+    public ColliderToggleManager toggleManager;
+
+    // Метод, который вызывается при нажатии на кнопку
+    public override void Interact()
+    {
+        if (toggleManager != null)
+        {
+            toggleManager.ToggleColliders(); // Вызываем метод для переключения состояния коллайдеров
+        }
+    }
+}
